2c561212db3ebd2ab56b0c7baf59a1ea8f143399
datng31 <dat.ng31@gmail.com>
2025-12-29 12:51:52 +0700

    update build system: - create new mission defs - modify build script compatible with chooseable mission

--- script_build.sh
+++ script_build.sh
@@ -1,15 +1,79 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# set -eu pipefail
 
-# Default simulation value
+# Defaults
 SIMULATION="i686-linux-gnu"
+MISSIONCONFIG="mission_sample"
+JOBS=16
 
-# Check if an argument is provided
-if [ $# -gt 0 ]; then
-    SIMULATION=$1
-fi
+usage() {
+  cat <<EOF
+Usage: $(basename "$0") [OPTIONS]
 
-echo "Using simulation: $SIMULATION"
+Options:
+  --simulation VALUE        Set SIMULATION (default: $SIMULATION)
+  --missionconfig VALUE     Set MISSIONCONFIG (default: $MISSIONCONFIG)
+  -j N                      Parallel jobs for make install (default: $JOBS)
+  -h, --help                Show this help and exit
+
+Examples:
+  $(basename "$0")
+  $(basename "$0") --simulation arm-linux-gnueabihf
+  $(basename "$0") --missionconfig demo
+  $(basename "$0") --missionconfig demo --simulation i686-linux-gnu -j 32
+
+Simulation available option:
+  i686-linux-gnu
+
+Mission available option:
+  mission_sample
+  mission_nomad
+EOF
+}
+
+# Parse args
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    --simulation=*)
+      SIMULATION="${1#*=}"
+      shift
+      ;;
+    --simulation)
+      SIMULATION="${2:-}"
+      shift 2
+      ;;
+    --missionconfig=*)
+      MISSIONCONFIG="${1#*=}"
+      shift
+      ;;
+    --missionconfig|--mission-config)
+      MISSIONCONFIG="${2:-}"
+      shift 2
+      ;;
+    -j)
+      JOBS="${2:-}"
+      shift 2
+      ;;
+    -j*)
+      JOBS="${1#-j}"
+      shift
+      ;;
+    *)
+      echo "Unknown argument: $1" >&2
+      usage >&2
+      exit 2
+      ;;
+  esac
+done
+
+echo "Using simulation:     $SIMULATION"
+echo "Using mission config: $MISSIONCONFIG"
+echo "Parallel jobs:        $JOBS"
 
 make distclean
-make SIMULATION=$SIMULATION prep
-make install
+make SIMULATION=$SIMULATION MISSIONCONFIG=$MISSIONCONFIG prep
+make -j"$JOBS" install
